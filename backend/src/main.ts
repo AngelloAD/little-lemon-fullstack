a2a695e21caf1fc377092612f2735d383a374c84
Angello Arenas <angelloarenas2013@gmail.com>
2026-07-14 07:20:35 -0500

    preparando configuracion de backend para render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,55 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuramos CORS para aceptar tanto tu entorno local como el futuro dominio de Render
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173', 
+      /https:\/\/.*\.onrender\.com$/ // Esto permite cualquier subdominio de Render de forma segura
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  // Render asignará un puerto en process.env.PORT. Si no existe (como en tu PC), usará el 3000.
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: 'http://localhost:5173',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   await app.listen(3000);
+// }
+// bootstrap();
